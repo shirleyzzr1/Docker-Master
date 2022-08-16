a95b6fe1b4c1a8a6a850699217c5a8aee48f0aaf
Kojo Welbeck <kojowelbeck2021@u.northwestern.edu>
2022-08-16 07:54:48 -0500

    In progress: feedback+result handling, emergency, heartbeat, robot_ip,

--- ros-packages/demo/demo/action_client.py
+++ ros-packages/demo/demo/action_client.py
@@ -32,42 +32,75 @@ class DemoActionClient(Node):
     """
     def __init__(self):
         """
-        
+        Subscribe to emergency alerts
+        Create client to report emergencies
+        Publish Heartbeat on a timer
+        Create OT2 action clinet to send OT2 goals and recieve progress feedback and results
+        TODO Not sending goals during emergency
         """
+
         super().__init__('demo_action_client')
+
+        ## Action client to be namespaced with action server
         self._action_client = ActionClient(self, OT2Job, 'OT2')
+
+        ## TODO replace where references are made and delete
         self.name = self.get_namespace()[1:]
-        # self.heartbeat_publisher = self.create_publisher()
+        
+        ## Set up Emergency tracking and service proxy (client)
+        self.emergency_sub = self.create_subscription(EmergencyAlert,'/emergency',self.emergency_callback,10)
+        self.emergency_flag = False ## TODO Maybe should default to True
+        # self.emergency_client = self.create_client(RaiseEmergency, "/raise_emergency")
 
-        self.srv = self.create_service(ExecuteJob,'execute_job',self.exectute_job_callback)
-
-        self.publisher_ = self.create_publisher(Heartbeat, 'state', 10)
-        self.emergency = self.create_subscription(EmergencyAlert,'/emergency',self.emergency_callback,10)
-
-
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-
+        ## Set up Heartbeat publishing on timer
+        heartbeat_timer_period = 0.5  # seconds
+        self.heartbeat_timer = self.create_timer(heartbeat_timer_period, self.heartbeat_timer_callback)
+        self.heartbeat_publisher = self.create_publisher(Heartbeat, 'state', 10) ## "heartbeat" to standard
+        self.heartbeat_msg = Heartbeat()
+        self.heartbeat_msg.header.src = self.name
+        # self._heartbeat_state = Heartbeat.IDLE
+        
+        ## Job service to trigger actions
+        self.execute_job_service = self.create_service(ExecuteJob,'execute_job',self.exectute_job_callback)
+        
+        ## Alert that the Action Server has been created
         self.get_logger().info("OT2 Action Client running!")
         self.get_logger().info("Send rc_path and pc_path with /execute_job service call")
     
-    def timer_callback(self):
+    def heartbeat_timer_callback(self):
+        """
+        Update the heartbeat's header timestamp and heartbeat state
+        and publish
         """
 
-        """
-        self.update_state()
-        # self.get_logger().info('Publishing: "%s"' % msg.data))
-
-    def update_state(self,info="None"):
-        msg = Heartbeat()
-        msg.header.src = self.name
-        msg.state = state.value
-        msg.message = info
-        self.publisher_.publish(msg)
+        # self.update_state()
+        # self.get_logger().info('Publishing: "%s"' % msg.data
+        self.heartbeat_msg.state = state.value
+        self.heartbeat_msg.message = ""
+        self.heartbeat.header.stamp = self.get_clock().now().to_msg
+        self.heartbeat_publisher.publish(self.heartbeat_msg)
     
+
+    def emergency_callback(self,msg):
+        """
+        Update private emergency status 
+        """
+        # if msg.message!="":
+            # self.get_logger().info(self.name + " client received an emergency alert: " + msg.message)
+        
+        if msg.isEmergency and not self.emergency_flag:
+            self.emergency_flag = True
+            self.get_logger().warn("{} action received an emergency alert: {}".format(self.get_fully_qualified_name(),msg.message)) 
+            ## TODO: Should include the source of error in the warn
+
+        if not msg.isEmergency and self.emergency_flag:
+            self.emergency_flag = False
+            self.get_logger().info("Emergency alert(s) cleared.")
+
+
     def exectute_job_callback(self,request,response):
         """
-
+        
         """
         rc_config = None
         pc_config = None
@@ -91,15 +124,16 @@ class DemoActionClient(Node):
         return response
 
 
-    def send_goal(self, pc_path=None, rc_path=None, protocol_config="", robot_config=""):
+    # def send_goal(self, pc_path=None, rc_path=None, protocol_config="", robot_config=""):
+    def send_goal(self, protocol_config="", robot_config=""):
         """
 
         """
         goal_msg = OT2Job.Goal()
-        if rc_path is not None:
-            goal_msg.job.rc_path = rc_path
-        if pc_path is not None:
-            goal_msg.job.pc_path = pc_path
+        # if rc_path is not None:
+        #     goal_msg.job.rc_path = rc_path
+        # if pc_path is not None:
+        #     goal_msg.job.pc_path = pc_path
 
         goal_msg.job.robot_config = robot_config
         goal_msg.job.protocol_config = protocol_config
@@ -142,9 +176,7 @@ class DemoActionClient(Node):
         # if not result.success:
         #     self.get_logger().info("Error Message: " + result.error_msg)
         # rclpy.shutdown()
-    def emergency_callback(self,msg):
-        if msg.message!="":
-            self.get_logger().info(self.name + " client received an emergency alert: " + msg.message)
+
 
 def main(args=None):
     rclpy.init(args=args)
